4bc2bb5c886eb355bd14834205bd5c4d79e9778c
myCAT <myCAT@olanto.org>
2013-09-13 16:29:13 +0200

    Version 2.3.02
    config unix, converter ...

--- prog/SimpleConverter/src/org/olanto/converter/ConverterPlugin.java
+++ prog/SimpleConverter/src/org/olanto/converter/ConverterPlugin.java
@@ -14,46 +14,63 @@ import org.artofsolving.jodconverter.process.PureJavaProcessManager;
  *
  */
 public class ConverterPlugin implements Callable {
-    private final static Logger _logger = Logger.getLogger(ConverterPlugin.class);
 
+    private final static Logger _logger = Logger.getLogger(ConverterPlugin.class);
     private String plugInName;
     private String plugInCommand;
     private String plugInProcess;
-    
-    public ConverterPlugin(String plugInName, String plugInCommand, String plugInProcess){
-            this.plugInName = plugInName;
-            this.plugInCommand = plugInCommand;
-            this.plugInProcess = plugInProcess;
+
+    public ConverterPlugin(String plugInName, String plugInCommand, String plugInProcess) {
+        this.plugInName = plugInName;
+        this.plugInCommand = plugInCommand;
+        this.plugInProcess = plugInProcess;
     }
-    
-    public String getPlugInName(){
+
+    public String getPlugInName() {
         return this.plugInName;
     }
-    
-    public String getPlugInCommand(){
+
+    public String getPlugInCommand() {
         return this.plugInCommand;
     }
-    
-    public String getPlugInProcess(){
+
+    public String getPlugInProcess() {
         return this.plugInProcess;
     }
-    
+
     @Override
-    public Integer call() throws Exception {      
-        Process p = new ProcessBuilder("cmd.exe", "/C",this.plugInCommand).start();
+    public Integer call() throws Exception {
+        Process p = new ProcessBuilder("cmd.exe", "/C", this.plugInCommand).start();
         return p.waitFor();
     }
-    
+
     public void stopProcess() {
-            ProcessManager process = new PureJavaProcessManager();
-            ProcessQuery query = new ProcessQuery(this.plugInCommand, this.plugInProcess);          
-             try {
-                Long pid = process.findPid(query);
-                _logger.info("Trying to kill plugin process: " + pid);
-                process.kill(null, pid);
-            } catch (IOException e1) {
-                _logger.error(e1.getMessage());
+//        try {
+//            ProcessManager process = new PureJavaProcessManager();
+//            ProcessQuery query = new ProcessQuery(this.plugInCommand, this.plugInProcess);
+//            Long pid = process.findPid(query);
+//            _logger.info("Trying to kill plugin process: " + pid);
+//            System.out.println("Trying to kill plugin process: " + pid);
+//            process.kill(null, pid);
+//        } catch (IOException e1) {
+//            _logger.error(e1.getMessage());
+//            System.out.println("Error Trying to kill plugin process: ");
+//        }
+//        
+        System.out.println("run :"+this.plugInCommand);
+        Process exec;
+        try {
+            exec = Runtime.getRuntime().exec("taskkill /F /IM "+this.plugInCommand);
+            try {
+                exec.waitFor();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
             }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        System.out.println("end of taskkill");
+ 
     }
-    
 }
--- prog/SimpleConverter/src/org/olanto/converter/PluginConverterFactory.java
+++ prog/SimpleConverter/src/org/olanto/converter/PluginConverterFactory.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import org.apache.log4j.Logger;
 
 /**
@@ -31,8 +33,22 @@ public class PluginConverterFactory extends AbstractConverterFactory {
                 conv.setSourceFileName(source.getAbsolutePath());
                 conv.setTargetFileName(target.getAbsolutePath());
                 ExecutorService executor = Executors.newFixedThreadPool(1);
-                Future<Integer> future = executor.submit(conv);             
-                int retValue=future.get();
+                Future<Integer> future = executor.submit(conv);
+                int retValue = -1;
+                try {
+                    retValue = future.get(300000, TimeUnit.MILLISECONDS);
+                } catch (TimeoutException e) {
+                    System.out.println("No response after 30 seconds" + retValue);
+                   try { 
+                       conv.stopProcess();
+                   
+                   } catch (Exception x) {
+                    System.out.println("error during stopProcess");
+                   }
+                   System.out.println("stopProcess ok");
+                   future.cancel(true);
+                    System.out.println("cancel");
+               }
                 executor.shutdownNow();
                 if (retValue == 0) {
                     this.success = true;
